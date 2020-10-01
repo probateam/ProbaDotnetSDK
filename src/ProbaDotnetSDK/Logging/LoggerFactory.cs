22ea8e76d105919e3e09b9480406d183fe24446b
hamed Mohammedi <hammedmohamadi@gmail.com>
2020-10-01 19:54:36 +0330

    adding logger

---
+++ src/ProbaDotnetSDK/Logging/LoggerFactory.cs
@@ -0,0 +1,33 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProbaDotnetSDK.Logging
+{
+    internal class LoggerFactory : IDisposable
+    {
+        public static ILogger CreateLogger(string filePath = @"./logs/")
+        {
+            var log = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.File(filePath)
+                .CreateLogger();
+            return log;
+        }
+
+        public ILogger Logger
+        {
+            get
+            {
+                if (Log.Logger is null) Log.Logger = CreateLogger();
+                return Log.Logger;
+            }
+        }
+
+        public void Dispose()
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
