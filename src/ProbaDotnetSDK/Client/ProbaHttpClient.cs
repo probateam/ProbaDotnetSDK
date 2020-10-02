967aa869d24f57530da24a1aa585e7c50a1e8ded
hamed Mohammedi <hammedmohamadi@gmail.com>
2020-10-02 15:33:14 +0330

    basic Post Request

--- src/ProbaDotnetSDK/Client/ProbaHttpClient.cs
+++ src/ProbaDotnetSDK/Client/ProbaHttpClient.cs
@@ -1,8 +1,12 @@
-﻿using Serilog;
+﻿using ProbaDotnetSDK.Services;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ProbaDotnetSDK.Client
 {
@@ -10,14 +14,36 @@ namespace ProbaDotnetSDK.Client
     {
         private ILogger Logger { get; }
         private HttpClient Client { get; }
+        private string SecretKet { get; }
+        private string ProjectId { get; }
+        private string BaseURL { get; }
+        private HmacService HmacService { get; }
 
-        public ProbaHttpClient(ILogger logger, HttpClient client)
+
+        private async Task<(HttpStatusCode statusCode, string content)> PostJsonRequestAsync(string url, string message, CancellationTokenSource cts)
         {
-            Logger = logger;
-            Client = client;
+            var request = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(message, Encoding.UTF8, "application/json")
+            };
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("hmac", HmacService.GenerateHmacSignature(SecretKet, message));
+            try
+            {
+                var response = await Client.SendAsync(request, cts.Token).ConfigureAwait(false);
+                var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                if (!response.IsSuccessStatusCode)
+                {
+                    Logger.Warning("Unsuccessfull http request response for request {URL} with {statusCode} statuscode and response: {serverResponse}", url, response.StatusCode, content);
+                }
+
+                return (response.StatusCode, content);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message, "Erorr while sending http request {URL}", url);
+                throw;
+            }
         }
-
-
 
     }
 }
