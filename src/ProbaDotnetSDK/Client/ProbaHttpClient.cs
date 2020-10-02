ee402e480a342ba0808b8333ff8d8107acbc69f5
hamed Mohammedi <hammedmohamadi@gmail.com>
2020-10-02 17:37:00 +0330

    end session

--- src/ProbaDotnetSDK/Client/ProbaHttpClient.cs
+++ src/ProbaDotnetSDK/Client/ProbaHttpClient.cs
@@ -37,6 +37,21 @@ namespace ProbaDotnetSDK.Client
             }
         }
 
+        public async Task<(bool sucess, HttpStatusCode statusCode)> EndSessionAsync(EndSessionViewModel endSessionViewModel)
+        {
+            try
+            {
+                var (sucess, statusCode, content) = await PostJsonRequestAsync($"{BaseURL}/{APIVersion}/Events/EndSession/{ProjectId}", endSessionViewModel.ToJson(), CancellationTokenSource);
+                if (sucess) return (sucess, statusCode);
+                //TODO: handdle errors
+                return (default, statusCode);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         private async Task<(bool sucess, HttpStatusCode statusCode, string content)> PostJsonRequestAsync(string url, string message, CancellationTokenSource cts)
         {
             var request = new HttpRequestMessage(HttpMethod.Post, url)
