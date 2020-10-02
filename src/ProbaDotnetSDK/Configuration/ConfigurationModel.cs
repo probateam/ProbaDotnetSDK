d34dee36caadf3a59693a64e9bcd9d93e988ea5a
hamed Mohammedi <hammedmohamadi@gmail.com>
2020-10-02 19:58:29 +0330

    configurations

---
+++ src/ProbaDotnetSDK/Configuration/ConfigurationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProbaDotnetSDK.Configuration
+{
+    internal class ConfigurationModel
+    {
+        public string BaseURL { get; set; }
+        public string ServerIPAddress { get; set; }
+        public int CurrentAPIVersion { get; set; }
+        public List<int> CompatibleAPIVesions { get; set; }
+    }
+}
