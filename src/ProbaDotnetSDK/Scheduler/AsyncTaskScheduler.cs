2a14a8c9d3854ffefdcca3034af7fb76c54665e3
hamed Mohammedi <hammedmohamadi@gmail.com>
2020-10-06 17:06:48 +0330

    Task scheduler

---
+++ src/ProbaDotnetSDK/Scheduler/AsyncTaskScheduler.cs
@@ -0,0 +1,97 @@
+﻿using ProbaDotnetSDK.Client;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProbaDotnetSDK.Scheduler
+{
+    internal class AsyncTaskScheduler
+    {
+        private ConcurrentQueue<TaskOrder> TaskList { get; }
+        private int ConcurrencyLevel { get; }
+        private SemaphoreSlim Semaphore { get; }
+        private CancellationTokenSource CancellationTokenSource { get; }
+        private ProbaHttpClient ProbaClient { get; }
+        public List<Exception> Exceptions { get; private set; }
+
+        private List<(int count, bool success, HttpStatusCode statusCode)> Responses { get; }
+        //public AsyncTaskScheduler(int concurrencyLevel)
+        //{
+        //    TaskList = new ConcurrentQueue<TaskOrder>();
+        //    ConcurrencyLevel = concurrencyLevel;
+        //    Semaphore = new SemaphoreSlim(0, ConcurrencyLevel);
+        //}
+
+        public void Schedule(TaskOrder order)
+        {
+            TaskList.Enqueue(order);
+            Semaphore.Release();
+        }
+
+        private async Task<TaskOrder> DequeueAsync()
+        {
+            await Semaphore.WaitAsync();
+
+            TaskList.TryDequeue(out var job);
+
+            return job;
+
+        }
+
+        private async void BackgroundProcessing()
+        {
+            while (!CancellationTokenSource.Token.IsCancellationRequested)
+            {
+                try
+                {
+                    var job = await DequeueAsync();
+                    bool sucess;
+                    HttpStatusCode statusCode;
+                    switch (job.Type)
+                    {
+                        case TaskType.SendAchievementEvent:
+                            (sucess, statusCode) = await ProbaClient.SendAchievementEventAsync(job.Achievement);
+                            break;
+                        case TaskType.SendAdvertisementEvent:
+                            (sucess, statusCode) = await ProbaClient.SendAdvertisementEventAsync(job.Advertisement);
+                            break;
+                        case TaskType.SendBusinessEvent:
+                            (sucess, statusCode) = await ProbaClient.SendBusinessEventAsync(job.Businesse);
+                            break;
+                        case TaskType.SendContentViewEvent:
+                            (sucess, statusCode) = await ProbaClient.SendContentViewEventAsync(job.ContentView);
+                            break;
+                        case TaskType.SendDesignEvent:
+                            (sucess, statusCode) = await ProbaClient.SendDesignEventAsync(job.DesignEvent);
+                            break;
+                        case TaskType.SendProgressionEvent:
+                            (sucess, statusCode) = await ProbaClient.SendProgressionEventAsync(job.Progression);
+                            break;
+                        case TaskType.SendSocialEvent:
+                            (sucess, statusCode) = await ProbaClient.SendSocialEventAsync(job.Social);
+                            break;
+                        case TaskType.SendTapEvent:
+                            (sucess, statusCode) = await ProbaClient.SendTapEventAsync(job.Tap);
+                            break;
+                        default:
+                            (sucess, statusCode) = (true, HttpStatusCode.NotFound);
+                            break;
+                    }
+                    Responses.Add((Responses.Count, sucess, statusCode));
+                }
+                catch (Exception ex)
+                {
+                    Exceptions.Add(ex);
+                }
+
+            }
+        }
+
+    }
+}
---
+++ src/ProbaDotnetSDK/Scheduler/TaskOrder.cs
@@ -0,0 +1,31 @@
+﻿using ProbaDotnetSDK.SharedClasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProbaDotnetSDK.Scheduler
+{
+    internal enum TaskType
+    {
+        SendAchievementEvent,
+        SendAdvertisementEvent,
+        SendBusinessEvent,
+        SendContentViewEvent,
+        SendDesignEvent,
+        SendProgressionEvent,
+        SendSocialEvent,
+        SendTapEvent
+    }
+    internal class TaskOrder
+    {
+        public TaskType Type { get; set; }
+        public AchievementEventViewModel Achievement { get; set; }
+        public AdvertisementEventViewModel Advertisement { get; set; }
+        public BusinessEventViewModel Businesse { get; set; }
+        public ContentViewEventViewModel ContentView { get; set; }
+        public DesignEventViewModel DesignEvent { get; set; }
+        public ProgressionEventViewModel Progression { get; set; }
+        public SocialEventViewModel Social { get; set; }
+        public TapEventViewModel Tap { get; set; }
+    }
+}
