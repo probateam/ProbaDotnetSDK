be44f46f6b16b7dac6b6df7595cfd6c42808e95c
hamed Mohammedi <hammedmohamadi@gmail.com>
2020-10-06 14:44:22 +0330

    remote configurations viewmodel

---
+++ src/ProbaDotnetSDK/SharedClasses/RemoteConfigurationsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProbaDotnetSDK.SharedClasses
+{
+    public class RemoteConfigurationsViewModel
+    {
+        public string ConfigKey { get; set; }
+
+        public string ConfigDescription { get; set; }
+
+        public string Value { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        #region OS Filter
+        public string OS { get; set; }
+        public bool OS_Exclude { get; set; }
+        #endregion
+
+        #region Country Filter
+        public string Country { get; set; }
+        public bool Country_Exclude { get; set; }
+        #endregion
+
+        #region Build Filter
+        public string Build { get; set; }
+        public bool Build_Exclude { get; set; }
+        #endregion
+
+        #region Class Filter
+        public string TimeClass { get; set; }
+        public bool TimeClass_Exclude { get; set; }
+        #endregion
+        public int Priority { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
